8aead279a9ce8ecbc7a03f5130f9d43da16bf7cd
SharliBeicon <carlostocinocubelo@gmail.com>
2017-05-22 18:57:40 +0200

    Modificación para detener la captación tras un tiempo determinado

--- pruebaClient.py
+++ pruebaClient.py
@@ -1,8 +1,10 @@
 import zmq
+import time
 
 context = zmq.Context()
 
 print("Conectando con el servidor..")
+time.sleep(1)
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
--- pruebaServer.py
+++ pruebaServer.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*-
+#Bibliotecas necesarias.
 import time
 import zmq
 import tweepy
@@ -6,6 +7,7 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 
+#Asignacion de las claves para la API de Twitter
 consumer_key = 'kRNH81VeTyDeCHo6v5x2EWbzm'
 consumer_secret = 'EHp5PUIqC6CXXEoofez5CDaNIIsBCrcISYCd7Sk4VDwunahUQ4'
 access_token = '243348295-R1plXZdp6n1PjqnZYhFinhwVHmj76zJqSuqVS7IT'
@@ -16,29 +18,39 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
+#Creación del socket para la comunicación con el cliente.
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
+#Clase recolectora de datos de Twitter.
 class MyListener(StreamListener):
+    def __init__(self, time_limit=120):
+        self.start_time = time.time()
+        self.limit = time_limit
+        self.saveFile = open('abcd.json', 'a')
+        super(MyListener, self).__init__()
+
     def on_data(self, data):
-        try:
-            with open('python.json', 'a') as f:
-                f.write(data)
-                return True
-        except BaseException as e:
-            print("Error on_data: %s" % str(e))
-        return True
+        if(time.time() - self.start_time) < self.limit:
+            self.saveFile.write(data)
+            self.saveFile.write('\n')
+            return True
+        else:
+            self.saveFile.close()
+            return False
 
-    def on_error(self, status):
-        print(status)
-        return True
-
+#Esperamos que un cliente establezca una conexión con el servidor.
+print "[*] Esperando conexión.."
 while True:
     message = socket.recv()
     time.sleep(1)
 
-    socket.send(b"[*]Información recolectandose correctamente")
+    socket.send(b"[*] Información recolectandose correctamente")
+    if(len(message) != 0):
+        break
 
-    twitter_stream = Stream(auth, MyListener())
-    twitter_stream.filter(track = [message])
+#Captamos los Tweets según el hashtag desado.
+print "[*] Hashtag captado"
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track = [message])
