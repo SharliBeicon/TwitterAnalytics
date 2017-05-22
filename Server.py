85524dc8948c521d90a59bfff1bf34ffc1423147
SharliBeicon <carlostocinocubelo@gmail.com>
2017-05-22 19:25:55 +0200

    División del proyecto en módulos más simples

---
+++ Server.py
@@ -0,0 +1,27 @@
+#-*- coding: utf-8 -*-
+#Bibliotecas necesarias.
+import time
+import zmq
+
+import TwitterRecolect
+from TwitterRecolect import *
+
+#Creación del socket para la comunicación con el cliente.
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+#Esperamos que un cliente establezca una conexión con el servidor.
+print "[*] Esperando conexión.."
+while True:
+    message = socket.recv()
+    time.sleep(1)
+
+    socket.send(b"[*] Información recolectandose correctamente")
+    if(len(message) != 0):
+        break
+
+#Captamos los Tweets según el hashtag desado.
+print "[*] Hashtag captado"
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track = [message])
--- TwitterRecolect.py
+++ TwitterRecolect.py
@@ -1,7 +1,4 @@
-#-*- coding: utf-8 -*-
-#Bibliotecas necesarias.
 import time
-import zmq
 import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -18,13 +15,8 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-#Creación del socket para la comunicación con el cliente.
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
-
 #Clase recolectora de datos de Twitter.
-class MyListener(StreamListener):
+class MyListener(tweepy.StreamListener):
     def __init__(self, time_limit=120):
         self.start_time = time.time()
         self.limit = time_limit
@@ -39,18 +31,3 @@ class MyListener(StreamListener):
         else:
             self.saveFile.close()
             return False
-
-#Esperamos que un cliente establezca una conexión con el servidor.
-print "[*] Esperando conexión.."
-while True:
-    message = socket.recv()
-    time.sleep(1)
-
-    socket.send(b"[*] Información recolectandose correctamente")
-    if(len(message) != 0):
-        break
-
-#Captamos los Tweets según el hashtag desado.
-print "[*] Hashtag captado"
-twitter_stream = Stream(auth, MyListener())
-twitter_stream.filter(track = [message])
